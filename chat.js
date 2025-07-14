ecc90e28c5681e75ea4a06e2f9a5fd15b4c8d4b5
Anastasya <NansyCup18@gmail.com>
2025-07-14 15:47:44 +0300

    Верстка меню-бургер, внедрение чат-бота

---
+++ chat.js
@@ -0,0 +1,11 @@
+var Tawk_API = Tawk_API || {},
+  Tawk_LoadStart = new Date();
+(function () {
+  var s1 = document.createElement('script'),
+    s0 = document.getElementsByTagName('script')[0];
+  s1.async = true;
+  s1.src = 'https://embed.tawk.to/6874f13fe452b41910d78ef7/1j04csomi';
+  s1.charset = 'UTF-8';
+  s1.setAttribute('crossorigin', '*');
+  s0.parentNode.insertBefore(s1, s0);
+})();
